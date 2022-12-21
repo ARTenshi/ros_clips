e05cf7ba77489df8579c1be9a2189daf1cf441db
luis_contreras <tenshi.unam@gmail.com>
2022-12-21 16:47:39 +0900

    try except added

--- ros_clips/scripts/ros_clipspy_bridge.py
+++ ros_clips/scripts/ros_clipspy_bridge.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+import rospy
+import rospkg
+
+import traceback
+
+#Initialize CLIPS Environment
+
 import clips
 import time, threading, logging, yaml, sys, os
 from io import StringIO
@@ -8,11 +15,9 @@ import std_msgs.msg
 from ros_clips.msg import StringArray
 from ros_clips.srv import RunPlanning, GetPrintMessage
 
-import rospy
-import rospkg
-
-#Initialize CLIPS Environment
 env = clips.Environment()
+env.fact_duplication = False
+
 
 #Start IO String
 log_stream = StringIO()
@@ -29,182 +34,231 @@ _clipsLock = threading.Lock()
 
 #Defs
 def resetLogStream():
-    log_stream.truncate(0)
-    log_stream.seek(0)
+    try:
+        log_stream.truncate(0)
+        log_stream.seek(0)
+    except:
+        rospy.logerr(traceback.print_exc())
 
 def getLogStream():
-    _log = []
-    _log = log_stream.getvalue().splitlines()
-    for i in range(len(_log)):
-        _log[i] = _log[i].rstrip('. ')
-    print(_log)
-    return _log
+    try:
+        _log = []
+        _log = log_stream.getvalue().splitlines()
+        for i in range(len(_log)):
+            _log[i] = _log[i].rstrip('. ')
+        print(_log)
+        return _log
+    except:
+        rospy.logerr(traceback.print_exc())
+        _log = []
+        return _log
 
 
 #Topics' callbacks
 def callbackCLIPSReset(data):
-    print ('\nFacts are being reset.')
-    _clipsLock.acquire()
-    
-    env.reset()
-    resetLogStream()
-    
-    _clipsLock.release()
-
-
-def callbackCLIPSClear(data):
-    print ("\nEnviroment is being cleared.")
-    _clipsLock.acquire()
-        
-    env.clear()
-    resetLogStream()
-    
-    _clipsLock.release()
-
-
-def callbackCLIPSLoadFile(data):
-    print ('\nLoading File...')
-    #filepath = data.data
-    filepath = os.path.abspath(os.path.expanduser(os.path.expandvars(data.data)))
-    if filepath[-3:] == 'clp':
+    try:
+        print ('\nFacts are being reset.')
         _clipsLock.acquire()
-        env.batch_star(filepath)
-        print ('File Loaded')
         
         env.reset()
         resetLogStream()
-        print ('Facts were reset')
         
         _clipsLock.release()
-        return
-    
-    path = os.path.dirname(os.path.abspath(filepath))
-    f = open(filepath, 'r')
-    line = f.readline()
-    
-    _clipsLock.acquire()
-    while line:
-        env.batch_star((path + os.sep + line).strip())
-        line = f.readline()
-    f.close()
-    
-    print ('Files Loaded')
-    
-    env.reset()
-    resetLogStream()
-    print ('Facts were reset')
-    
-    _clipsLock.release()
+    except:
+        rospy.logerr(traceback.print_exc())
 
+
+def callbackCLIPSClear(data):
+    try:
+        print ("\nEnviroment is being cleared.")
+        _clipsLock.acquire()
+            
+        env.clear()
+        resetLogStream()
+        
+        _clipsLock.release()
+    except:
+        rospy.logerr(traceback.print_exc())
+
+
+def callbackCLIPSLoadFile(data):
+    try:
+        print ('\nLoading File...')
+        #filepath = data.data
+        filepath = os.path.abspath(os.path.expanduser(os.path.expandvars(data.data)))
+        if filepath[-3:] == 'clp':
+            _clipsLock.acquire()
+            env.batch_star(filepath)
+            print ('File Loaded')
+            
+            env.reset()
+            resetLogStream()
+            print ('Facts were reset')
+            
+            _clipsLock.release()
+            return
+        elif filepath[-3:] == 'dat':
+            path = os.path.dirname(os.path.abspath(filepath))
+            f = open(filepath, 'r')
+            line = f.readline()
+            
+            _clipsLock.acquire()
+            while line:
+                env.batch_star((path + os.sep + line).strip())
+                line = f.readline()
+            f.close()
+            
+            print ('Files Loaded')
+            
+            env.reset()
+            resetLogStream()
+            print ('Facts were reset')
+            
+            _clipsLock.release()
+        else:
+            print ('Invalid file')
+    except:
+        rospy.logerr(traceback.print_exc())
 
 #####
 def callbackBuildRule(data):
-    print ('\nBuilding Rule: ' + data.data)
-    _clipsLock.acquire()
-    
-    env.build(data.data)
-    
-    _clipsLock.release()
+    try:
+        print ('\nBuilding Rule: ' + data.data)
+        _clipsLock.acquire()
+        
+        env.build(data.data)
+        
+        _clipsLock.release()
+    except:
+        rospy.logerr(traceback.print_exc())
+
 
 def callbackBuildTemplate(data):
-    print ('\nBuilding Template: ' + data.data)
-    _clipsLock.acquire()
-    
-    env.build(data.data)
-    
-    _clipsLock.release()
+    try:
+        print ('\nBuilding Template: ' + data.data)
+        _clipsLock.acquire()
+        
+        env.build(data.data)
+        
+        _clipsLock.release()
+    except:
+        rospy.logerr(traceback.print_exc())
+
 
 def callbackBuildFacts(data):
-    print ('\nBuilding Facts: ' + data.data)
-    _clipsLock.acquire()
-    
-    env.build(data.data)
-    
-    _clipsLock.release()
+    try:
+        print ('\nBuilding Facts: ' + data.data)
+        _clipsLock.acquire()
+        
+        env.build(data.data)
+        
+        _clipsLock.release()
+    except:
+        rospy.logerr(traceback.print_exc())
+
 
 def callbackLoadFact(data):
-    print ('\nLoading Fact: ' + data.data)
-    _clipsLock.acquire()
-    
-    env.load_facts(data.data)
-    
-    _clipsLock.release()
+    try:
+        print ('\nLoading Fact: ' + data.data)
+        _clipsLock.acquire()
+        
+        env.load_facts(data.data)
+        
+        _clipsLock.release()
+    except:
+        rospy.logerr(traceback.print_exc())
 
 
 #Services' callbacks
 def callbackRunPlanningService(req):
-
-    print ("\nPlanning and Running:")
-    _clipsLock.acquire()
-    
-    limit = req.steps
-    if limit is not None and int(limit) > 0:
-        limit = int(limit)+1
-    else:
-        limit = None
-    
-    resetLogStream()
-    env.run(limit)
-    _p = getLogStream()
-    plan = StringArray(_p)
-    
-    _clipsLock.release()
-    return plan 
+    try:
+        print ("\nPlanning and Running:")
+        _clipsLock.acquire()
+        
+        limit = req.steps
+        if limit is not None and int(limit) > 0:
+            limit = int(limit)+1
+        else:
+            limit = None
+        
+        resetLogStream()
+        env.run(limit)
+        _p = getLogStream()
+        plan = StringArray(_p)
+        
+        _clipsLock.release()
+        return plan 
+    except:
+        rospy.logerr(traceback.print_exc())
+        _p = []
+        return StringArray(_p)
 
 def callbackGetFactsService(req):
+    try:
+        print ("\nGetting Facts:")
+        _clipsLock.acquire()
 
-    print ("\nGetting Facts:")
-    _clipsLock.acquire()
+        facts = []
+        for _fact in env.facts():
+            f = str(_fact.index) + " " + str(_fact)
+            f = f.lstrip()
+            #_id,_f = f.split(maxsplit=1)
+            if (len(f)>0):
+                facts.append(f)
 
-    facts = []
-    for _fact in env.facts():
-        f = str(_fact.index) + " " + str(_fact)
-        f = f.lstrip()
-        #_id,_f = f.split(maxsplit=1)
-        if (len(f)>0):
-            facts.append(f)
-
-    print(facts)
-    data = StringArray(facts)
-    
-    _clipsLock.release()
-    return data 
+        print(facts)
+        data = StringArray(facts)
+        
+        _clipsLock.release()
+        return data 
+    except:
+        rospy.logerr(traceback.print_exc())
+        data = []
+        return StringArray(data)
 
 def callbackGetRulesService(req):
+    try:
+        print ("\nGetting Rules:")
+        _clipsLock.acquire()
 
-    print ("\nGetting Rules:")
-    _clipsLock.acquire()
+        rules = []
+        for _rule in env.rules():
+            r = str(_rule)
+            r = r.lstrip()
+            if (len(r)>0):
+                rules.append(r)
 
-    rules = []
-    for _rule in env.rules():
-        r = str(_rule)
-        r = r.lstrip()
-        if (len(r)>0):
-            rules.append(r)
-
-    print(rules)
-    data = StringArray(rules)
-    
-    _clipsLock.release()
-    return data 
+        print(rules)
+        data = StringArray(rules)
+        
+        _clipsLock.release()
+        return data 
+    except:
+        rospy.logerr(traceback.print_exc())
+        data = []
+        return StringArray(data)
 
 def callbackGetTemplatesService(req):
+    try:
+        print ("\nGetting Templates:")
+        _clipsLock.acquire()
 
-    print ("\nGetting Templates:")
-    _clipsLock.acquire()
+        templates = []
+        for _templates in env.templates():
+            t = str(_templates)
+            t = t.lstrip()
+            if (len(t)>0):
+                templates.append(t)
 
-    templates = []
-    for _templates in env.templates():
-        t = str(_templates)
-        t = t.lstrip()
-        if (len(t)>0):
-            templates.append(t)
-
-    print(templates)
-    data = StringArray(templates)
-    
-    _clipsLock.release()
-    return data 
+        print(templates)
+        data = StringArray(templates)
+        
+        _clipsLock.release()
+        return data 
+    except:
+        rospy.logerr(traceback.print_exc())
+        data = []
+        return StringArray(data)
 
 #Main
 def main():
